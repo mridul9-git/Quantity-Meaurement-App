34d1c7e3c119a5981670a90b6cba3354d59ccecf
your GitHub shobhitag001 <your GitHub shobhitag001@gmail.com>
2026-04-27 09:33:03 +0530

    UC2: Inch equality implemented similar to Feet

--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -24,4 +24,33 @@ class QuantityMeasurementAppTest {
 
         assertTrue(f1.equals(f1));
     }
+    @Test
+    void testInchEquality_SameValue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void testInchEquality_DifferentValue() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+        QuantityMeasurementApp.Inch i2 = new QuantityMeasurementApp.Inch(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void testInchEquality_NullComparison() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void testInchEquality_SameReference() {
+        QuantityMeasurementApp.Inch i1 = new QuantityMeasurementApp.Inch(1.0);
+
+        assertTrue(i1.equals(i1));
+    }
 }

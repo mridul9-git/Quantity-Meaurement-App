34fa7d5a515f847db91512a3e566ea6f6b78033b
your GitHub shobhitag001 <your GitHub shobhitag001@gmail.com>
2026-04-27 09:38:35 +0530

    UC2: Inch equality implemented

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -20,5 +20,25 @@ public class QuantityMeasurementApp {
             return Double.compare(this.value, other.value) == 0;
         }
     }
+    public static class Inch {
+
+        private final double value;
+
+        public Inch(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inch other = (Inch) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
 
 }
